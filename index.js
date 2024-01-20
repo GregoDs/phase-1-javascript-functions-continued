aeaf69a7c8eda9b68da3a6856f8bee8578e4a844
GregoDs <gregorykago007@gmail.com>
2024-01-20 15:35:48 +0300

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    // Use a conditional statement to check if activity is provided
+    if (activity === "roller-skate") {
+      return `This Saturday, I want to ${activity}!`;
+    } else {
+      return `This Saturday, I want to ${activity}!`;
+    }
+  }
+
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+
+ 
+  function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
